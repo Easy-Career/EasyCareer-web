868dcd8cc4243f79181fbde74ef5561b931f1fc1
AdrianGMS <adriangmoscols@gmail.com>
2023-06-26 04:49:13 -0500

    agregado filtros dinamicos con base de datos db.json a la pantallad e carreras profesionales

--- EasyCareer/public/carreras.js
+++ EasyCareer/public/carreras.js
@@ -1,255 +1,63 @@
 function seleccionarContenedor(contenedor) {
-  contenedor.classList.toggle("seleccionado");
-  
-  let contenedoresSeleccionados = [];
-  let contenedores = document.querySelectorAll('.carrera.seleccionado');
-  
-  contenedores.forEach(contenedor => {
-    contenedoresSeleccionados.push(contenedor.textContent.trim());
-  });
-  
-  let basico = document.querySelector('.basico');
-  let salario = document.querySelector('.salario');
-  let requisitos = document.querySelector('.requisitos');
-  let popular = document.querySelector('.popular');
-  let ubicacion = document.querySelector('.ubicacion');
-  let dificultad = document.querySelector('.dificultad');
-  let todos = document.querySelector('.todos');
-  
-  let salarioRequisito = document.querySelector('.salarioRequisito');
-  let salarioPopular = document.querySelector('.salarioPopular');
-  let salarioUbicacion = document.querySelector('.salarioUbicacion');
-  let salarioDificultad = document.querySelector('.salarioDificultad');
-  let requisitoPopular = document.querySelector('.requisitoPopular');
-  let requisitoUbicacion = document.querySelector('.requisitoUbicacion');
-  let requisitoDificultad = document.querySelector('.requisitoDificultad');
-  let popularUbicacion = document.querySelector('.popularUbicacion');
-  let popularDificultad = document.querySelector('.popularDificultad');
-  let ubicacionDificultad = document.querySelector('.ubicacionDificultad');
+  var seleccionado = contenedor.getElementsByTagName('h3')[0].innerHTML;
+  var resultContainer = document.getElementById('result-container');
+  var basicoList = document.getElementById('basico-list');
 
-  let salarioRequisitoPopular = document.querySelector('.salarioRequisitoPopular');
-  let requisitoPopularUbicacion = document.querySelector('.requisitoPopularUbicacion');
-  let salarioUbicacionDificultad = document.querySelector('.salarioUbicacionDificultad');
-  let requisitoPopularDificultad = document.querySelector('.requisitoPopularDificultad');
-  let salarioPopularDificultad = document.querySelector('.salarioPopularDificultad');
-  let requisitoUbicacionDificultad = document.querySelector('.requisitoUbicacionDificultad');
-  let salarioRequisitoUbicacion = document.querySelector('.salarioRequisitoUbicacion');
-  let popularUbicacionDificultad = document.querySelector('.popularUbicacionDificultad');
-  let salarioPopularUbicacion = document.querySelector('.salarioPopularUbicacion');
-  let salarioRequisitoDificultad = document.querySelector('.salarioRequisitoDificultad');
+  var isSelected = contenedor.classList.contains('seleccionado');
 
-  let salarioRequisitoPopularUbicacion = document.querySelector('.salarioRequisitoPopularUbicacion');
-  let salarioRequisitoPopularDificultad = document.querySelector('.salarioRequisitoPopularDificultad');
-  let salarioRequisitoUbicacionDificultad = document.querySelector('.salarioRequisitoUbicacionDificultad');
-  let salarioPopularUbicacionDificultad = document.querySelector('.salarioPopularUbicacionDificultad');
-  let requisitoPopularUbicacionDificultad = document.querySelector('.requisitoPopularUbicacionDificultad');
-
-
-
-
-  let main = document.querySelector('main');
-
-  if (contenedoresSeleccionados.length === 1) {
-    if (contenedoresSeleccionados.includes('Salario')) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salario.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.includes('Requisitos')) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      requisitos.style.display = 'block';
-    }
-    else if (contenedoresSeleccionados.includes('Más Populares')) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      popular.style.display = 'block';
-    }
-    else if (contenedoresSeleccionados.includes('Ubicación')) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      ubicacion.style.display = 'block';
-    }
-    else if (contenedoresSeleccionados.includes('Dificultad')) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      dificultad.style.display = 'block';
-    }
-  } 
-  else if (contenedoresSeleccionados.length === 2) {
-    if (contenedoresSeleccionados.every(item => ['Salario', 'Requisitos'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioRequisito.style.display = 'block';
-    }
-    else if (contenedoresSeleccionados.every(item => ['Salario', 'Más Populares'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioPopular.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Salario', 'Ubicación'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioUbicacion.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Salario', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Más Populares', 'Requisitos'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      requisitoPopular.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Ubicación', 'Requisitos'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      requisitoUbicacion.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Dificultad', 'Requisitos'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      requisitoDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Más Populares', 'Ubicación'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      popularUbicacion.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Más Populares', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      popularDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Dificultad', 'Ubicación'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      ubicacionDificultad.style.display = 'block';
-    } 
+  if (isSelected) {
+    contenedor.classList.remove('seleccionado');
+  } else {
+    contenedor.classList.add('seleccionado');
   }
-  else if (contenedoresSeleccionados.length === 3) {
-    if (contenedoresSeleccionados.every(item => ['Salario', 'Requisitos', 'Más Populares'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
+
+  // Obtener todos los contenedores seleccionados
+  var contenedoresSeleccionados = Array.from(document.getElementsByClassName('carrera seleccionado'));
+  
+  fetch('http://localhost:3000/carreras')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var carreras = data;
+      var resultHTML = '';
+
+      carreras.forEach(function(carrera) {
+        var nombre = carrera.nombre;
+        var filtro = '';
+        // Combinar los datos de los contenedores seleccionados
+        contenedoresSeleccionados.forEach(function(cont) {
+          var seleccionado = cont.getElementsByTagName('h3')[0].innerHTML;
+
+          if (seleccionado === 'Salario') {
+            filtro += carrera.salario + ' ';
+          } else if (seleccionado === 'Requisitos') {
+            filtro += carrera.requisitos + ' ';
+          } else if (seleccionado === 'Más Populares') {
+            filtro += carrera.popular + ' ';
+          } else if (seleccionado === 'Ubicación') {
+            filtro += carrera.ubicacion + ' ';
+          } else if (seleccionado === 'Dificultad') {
+            filtro += carrera.dificultad + ' ';
+          }
+        });
+
+        if (filtro.trim() !== '') {
+          resultHTML += '<li>' + nombre + ": " + filtro.trim() + '</li>';
+        }
+
       });
-      salarioRequisitoPopular.style.display = 'block';
-    }
-    else if (contenedoresSeleccionados.every(item => ['Requisitos', 'Más Populares', 'Ubicación'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      requisitoPopularUbicacion.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Salario', 'Ubicación', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioUbicacionDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Requisitos', 'Más Populares', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      requisitoPopularDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Salario', 'Más Populares', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioPopularDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Requisitos', 'Ubicación', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      requisitoUbicacionDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Salario', 'Requisitos', 'Ubicación'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioRequisitoUbicacion.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Más Populares', 'Ubicación', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      popularUbicacionDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Salario', 'Más Populares', 'Ubicación'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioPopularUbicacion.style.display = 'block';
-    } 
-    if (contenedoresSeleccionados.every(item => ['Salario', 'Requisitos', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioRequisitoDificultad.style.display = 'block';
-    } 
-  }
-  else if (contenedoresSeleccionados.length === 4) {
-    if (contenedoresSeleccionados.every(item => ['Salario', 'Requisitos', 'Más Populares', 'Ubicación'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioRequisitoPopularUbicacion.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Salario', 'Requisitos', 'Más Populares', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioRequisitoPopularDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Salario', 'Requisitos', 'Ubicación', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioRequisitoUbicacionDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Salario', 'Más Populares', 'Ubicación', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      salarioPopularUbicacionDificultad.style.display = 'block';
-    } 
-    else if (contenedoresSeleccionados.every(item => ['Requisitos', 'Más Populares', 'Ubicación', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      requisitoPopularUbicacionDificultad.style.display = 'block';
-    } 
-  }
-  else if (contenedoresSeleccionados.length === 5) {
-    if (contenedoresSeleccionados.every(item => ['Salario', 'Requisitos', 'Más Populares', 'Ubicación', 'Dificultad'].includes(item))) {
-      main.querySelectorAll('ul').forEach(ul => {
-        ul.style.display = 'none';
-      });
-      todos.style.display = 'block';
-    } 
-  }
-  else {
-    main.querySelectorAll('ul').forEach(ul => {
-      ul.style.display = 'none';
+
+      resultContainer.innerHTML = '<ul class="basico">' + resultHTML + '</ul>';
+
+      // Verificar si hay al menos un contenedor seleccionado
+      if (contenedoresSeleccionados.length > 0) {
+        basicoList.style.display = 'none';
+      } else {
+        basicoList.style.display = 'block';
+      }
+    })
+    .catch(function(error) {
+      console.log('Error al leer el archivo db.json:', error);
     });
-    basico.style.display = 'block';
-
-  }
 }
